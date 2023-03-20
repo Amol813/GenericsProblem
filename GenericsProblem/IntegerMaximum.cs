4119b73a45c19f93485788679be8b764d8ffdc72
unknown <amoljadhav>
2023-03-20 20:09:53 +0530

    [Amol]Add:ThreeIntMax

---
+++ GenericsProblem/IntegerMaximum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsProblem
+{
+    public class IntegerMaximum
+    {
+        public void FindIntNum(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                Console.WriteLine("a is max");
+            }
+             if (b >= a && b >= c)
+             {
+                Console.WriteLine("b is max");
+             }
+           else 
+           {
+                Console.WriteLine("c is max");
+           }
+            
+        }
+    }
+}
--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace GenericsProblem
+﻿using System;
+
+namespace GenericsProblem
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Three Integers Max program!");
+
+            IntegerMaximum integerMaximum = new IntegerMaximum();
+
+
+            Console.WriteLine(" Case 1:");
+            integerMaximum.FindIntNum(50, 15, 9);
+
+
+            Console.WriteLine(" Case 2:");
+            integerMaximum.FindIntNum(15, 50, 9);
+
+
+            Console.WriteLine(" Case 3:");
+            integerMaximum.FindIntNum(15, 9, 50);
+
+            Console.ReadLine();
         }
     }
 }
